9cfb83faec33fda301b7bdca712d78f91101932e
RohanScripts <shelarrohan1810@gmail.com>
2024-06-20 21:11:58 +0530

    feat: Update Todo component styling
    Refactor the Todo component to use a more visually appealing and responsive layout. This improves the overall user experience and makes the todo list application more visually appealing.

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Todo = () => {
-  return <div className=""></div>;
+  return (
+    <div className="bg-white place-self-center w-96 max-w-md flex flex-col p-7  min-h-[450px] rounded-lg"></div>
+  );
 };
 
 export default Todo;
