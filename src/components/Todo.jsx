0e0c9b580b241da860c266e54553609f5dbf6100
RohanScripts <shelarrohan1810@gmail.com>
2024-06-23 16:34:19 +0530

    feat: Refactor Todo component layout and styling
    Refactor the Todo component to improve the user experience and visual appeal. This includes updating the layout and styling of the component to be more visually appealing and responsive. The changes enhance the overall user experience of the todo list application.

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,53 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import todo_icon from "../assets/todo_icon.png";
 import TodoList from "./TodoList";
 
 const Todo = () => {
+  const [todos, setTodos] = useState(
+    localStorage.getItem("todos")
+      ? JSON.parse(localStorage.getItem("todos"))
+      : []
+  );
+
   const inputRef = useRef();
 
   const addTodo = () => {
     const inputText = inputRef.current.value.trim();
+
+    if (inputText === "") {
+      return null;
+    }
+
+    const newTodo = {
+      id: Date.now(),
+      text: inputText,
+      isCompleted: false,
+    };
+
+    setTodos((prev) => [...prev, newTodo]);
+    inputRef.current.value = "";
   };
+
+  const deleteTodo = (id) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((eachTodo) => eachTodo.id !== id);
+    });
+  };
+
+  const toggle = (id) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((eachTodo) => {
+        if (eachTodo.id === id) {
+          return { ...eachTodo, isCompleted: !eachTodo.isCompleted };
+        }
+        return eachTodo;
+      });
+    });
+  };
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <div className="bg-white opacity-75 place-self-center w-96 max-w-md flex flex-col p-7  min-h-[450px] rounded-lg border-4 border-inherit">
@@ -19,6 +59,11 @@ const Todo = () => {
       {/* input */}
       <div className="flex items-center mt-4 mb-4 opacity-100 bg-gray-300 rounded-full ">
         <input
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              addTodo();
+            }
+          }}
           ref={inputRef}
           className="bg-transparent outline-none  h-10  pl-4 flex-1 "
           type="text"
@@ -34,7 +79,16 @@ const Todo = () => {
       </div>
       {/* todo items */}
       <div>
-        <TodoList text="xyz"></TodoList>
+        {todos.map((eachTodo, index) => (
+          <TodoList
+            key={index}
+            text={eachTodo.text}
+            deleteTodo={deleteTodo}
+            id={eachTodo.id}
+            isCompleted={eachTodo.isCompleted}
+            toggle={toggle}
+          ></TodoList>
+        ))}
       </div>
     </div>
   );
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,14 +3,26 @@ import tick from "../assets/tick.png";
 import not_tick from "../assets/not_tick.png";
 import deleteIcon from "../assets/delete.png";
 
-const TodoList = ({ text }) => {
+const TodoList = ({ text, id, isCompleted, deleteTodo, toggle }) => {
   return (
     <div className="flex items-center">
       <div className="flex items-center cursor-pointer flex-1 gap-2">
-        <img src={tick} alt="" className="w-5 " />
-        <p className="text-slate-800">{text}</p>
+        <img
+          onClick={() => toggle(id)}
+          src={isCompleted ? tick : not_tick}
+          alt=""
+          className="w-5 "
+        />
+        <p className={`text-slate-800  ${isCompleted ? "line-through" : ""}`}>
+          {text}
+        </p>
       </div>
-      <img src={deleteIcon} alt="" className="w-3 cursor-pointer" />
+      <img
+        src={deleteIcon}
+        onClick={() => deleteTodo(id)}
+        alt=""
+        className="w-3 cursor-pointer hover:w-5 duration-300"
+      />
     </div>
   );
 };
